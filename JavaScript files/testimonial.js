e60188ff7c7ab1a09a8a964b7201825121d2e564
pshar-207 <p.shar.18.12.2000@gmail.com>
2023-11-04 14:26:35 +0530

    Add 'Testimonial section' in Index2

--- JavaScript files/testimonial.js
+++ JavaScript files/testimonial.js
@@ -9,21 +9,21 @@ document.addEventListener("DOMContentLoaded", function () {
     this.classList.add("active");
   };
   btn[1].onclick = function () {
-    slide.style.transform = "translateX(-800px)";
+    slide.style.transform = "translateX(-40rem)";
     for (i = 0; i < 4; i++) {
       btn[i].classList.remove("active");
     }
     this.classList.add("active");
   };
   btn[2].onclick = function () {
-    slide.style.transform = "translateX(-1600px)";
+    slide.style.transform = "translateX(-80rem)";
     for (i = 0; i < 4; i++) {
       btn[i].classList.remove("active");
     }
     this.classList.add("active");
   };
   btn[3].onclick = function () {
-    slide.style.transform = "translateX(-2400px)";
+    slide.style.transform = "translateX(-120rem)";
     for (i = 0; i < 4; i++) {
       btn[i].classList.remove("active");
     }
