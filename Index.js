819e03fbba6a4bb71eb7dd89bb02e6f0b489207f
pshar-207 <p.shar.18.12.2000@gmail.com>
2023-11-09 20:45:54 -0800

    Navbar change color on Hover

--- Index.js
+++ Index.js
@@ -7,53 +7,74 @@ function loadAndInsertContent(url, placeholderId) {
     .then((content) => {
       // Insert the fetched content into the placeholder
       placeholder.innerHTML = content;
-    })
-    .catch((error) => {
-      console.error("Error loading content:", error);
-    });
-}
-
-// Function to load HTML content and insert it into a placeholder
-function loadAndInsertContent(url, placeholderId) {
-  const placeholder = document.getElementById(placeholderId);
-
-  fetch(url)
-    .then((response) => response.text())
-    .then((content) => {
-      // Insert the fetched content into the placeholder
-      placeholder.innerHTML = content;
 
       // After content is loaded, change the background color
+      // Call the function to change the background color on scroll
       changeNavbarBackgroundColorOnScroll();
     })
     .catch((error) => {
       console.error("Error loading content:", error);
     });
-}
+ }
+
 // Function to change Navbar background color on scroll
-function changeNavbarBackgroundColorOnScroll() {
+// function changeNavbarBackgroundColorOnScroll() {
+//   const navbarContainer = document.getElementById("navbar-container");
+//   const shopbg = document.getElementById("shop_bg");
+//   const categorybg = document.getElementById("category_bg");
+
+//   window.addEventListener("scroll", () => {
+//     console.log("Scroll position:", window.scrollY);
+//     if (window.scrollY > 0) {
+//       // console.log("Scrolling down.");
+//       navbarContainer.style.backgroundColor = "white";
+//       shopbg.style.backgroundColor = "white";
+//       categorybg.style.backgroundColor = "white";
+//     } else {
+//       // console.log("At the top.");
+//       navbarContainer.style.backgroundColor = "transparent";
+//       shopbg.style.backgroundColor = "transparent";
+//       categorybg.style.backgroundColor = "transparent";
+//     }
+//   });
+//  }
+
+ function changeNavbarBackgroundColorOnScroll() {
   const navbarContainer = document.getElementById("navbar-container");
   const shopbg = document.getElementById("shop_bg");
   const categorybg = document.getElementById("category_bg");
 
+  // Function to handle hover effect
+  function handleNavbarHover() {
+    navbarContainer.style.backgroundColor = "white";
+    shopbg.style.backgroundColor = "white";
+    categorybg.style.backgroundColor = "white";
+  }
+
+  // Function to handle mouseout event
+  function handleNavbarMouseOut() {
+    navbarContainer.style.backgroundColor = "transparent";
+    shopbg.style.backgroundColor = "transparent";
+      categorybg.style.backgroundColor = "transparent";
+  }
+
+  // Add event listeners for hover effect
+  navbarContainer.addEventListener("mouseover", handleNavbarHover);
+  navbarContainer.addEventListener("mouseout", handleNavbarMouseOut);
+
   window.addEventListener("scroll", () => {
     console.log("Scroll position:", window.scrollY);
     if (window.scrollY > 0) {
-      // console.log("Scrolling down.");
       navbarContainer.style.backgroundColor = "white";
       shopbg.style.backgroundColor = "white";
       categorybg.style.backgroundColor = "white";
     } else {
-      // console.log("At the top.");
       navbarContainer.style.backgroundColor = "transparent";
       shopbg.style.backgroundColor = "transparent";
       categorybg.style.backgroundColor = "transparent";
     }
   });
 }
-
-// Call the function to change the background color on scroll
-changeNavbarBackgroundColorOnScroll();
 
 // Load header and footer using the function
 loadAndInsertContent("Header.html", "header-placeholder");
